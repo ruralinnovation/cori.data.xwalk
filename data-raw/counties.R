ad2700356d11ec338c62a126469daea307494a91
Olivier Leroy <leroy.oli@proton.me>
2024-10-28 16:20:25 -0400

    trap to get the tract with geo

--- data-raw/counties.R
+++ data-raw/counties.R
@@ -1,3 +1,10 @@
 ## code to prepare `counties` dataset goes here
 
+connecticut_xwalk <- paste0("https://www2.census.gov/geo/docs/",
+                            "reference/ct_change/ct_cou_to_cousub_crosswalk.txt")
+
+ct <- read.csv(connecticut_xwalk, sep = "|", header = TRUE,
+               colClasses = "character",
+               nrows = length(readLines(connecticut_xwalk)) - 16)
+
 usethis::use_data(counties, overwrite = TRUE)
